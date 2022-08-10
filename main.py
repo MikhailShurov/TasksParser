d0d686521da2d7fc5d7729b906b0901ce4169781
Mikhail <mixailshurov@gmail.com>
2022-08-10 12:29:58 +0300

    Restructured kwork parser

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from parsers import parse_weblancer
-# from parsers import parse_kwork
+from parsers import parse_kwork
 
 if __name__ == '__main__':
     wl = parse_weblancer.Weblancer()
     wl.parse()
+
+    kw = parse_kwork.Kwork()
+    kw.parse()
 
--- parsers/parse_kwork.py
+++ parsers/parse_kwork.py
@@ -1,32 +1,27 @@
-from selenium.webdriver.common.by import By
+import save.saved_links as saved_links
+import save.save_system as save_system
 
-import saved_links
-import save_system
+import requests
+from bs4 import BeautifulSoup
 
-from time import sleep
+import TelegramClient
 
 
 class Kwork:
-    def __init__(self, browser):
-        self.browser = browser
-
+    def __init__(self):
         self.current_links = []
         self.latest_links = saved_links.kwork_last_links
+        self.TC = TelegramClient.TelegramClient()
 
     def parse(self):
-        self.current_links = []
-        self.browser.get('https://kwork.ru/projects?c=41')
-        sleep(10)
-        tasks = self.browser.find_elements(By.CSS_SELECTOR, '.want-card')
-        message = '''Kwork, новые задания в питоне:
-'''
-        for task in tasks:
-            link = task.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        self.current_links = [link.find("a").get("href") + '/view'for link in BeautifulSoup(requests.get('https://kwork.ru/projects?c=41').text, 'lxml').find_all("div", "wants-card__left")]
+        message = '''Нашел новые заказы на kwork:'''
+        for link in self.current_links:
             if link not in self.latest_links:
-                message += '\n'
-                message += link
-            self.current_links.append(link)
+                message += f'\n\n{link}'
 
         self.latest_links = self.current_links.copy()
         save_system.save(2, self.latest_links)
 
+        if message != '''Нашел новые заказы на kwork:''':
+            self.TC.send_text_message(message)
